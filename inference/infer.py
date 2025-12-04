485f5649d49f6800c38eeb273554e66688cefb7f
wingchiuloong <774817136@qq.com>
2025-12-04 19:12:32 +0800

    增加PubmedQA

--- inference/infer.py
+++ inference/infer.py
@@ -38,6 +38,7 @@ if project_root not in sys.path:
 from inference.prompt import create_prompt
 from load_datasets.faitheval import load_faitheval
 from load_datasets.hotpotqa import load_hotpotqa
+from load_datasets.pubmedqa import load_pubmedqa
 
 
 def call_model(client: OpenAI, model_name: str, prompt: str, max_tokens: int = 4096, temperature: float = 0.7, top_p=0.95) -> str:
@@ -149,6 +150,8 @@ def run_inference(
         dataset = load_hotpotqa()
     elif dataset_name == "faitheval":
         dataset = load_faitheval()
+    elif dataset_name == "pubmedqa":
+        dataset = load_pubmedqa()
     else:
         raise ValueError(f"不支持的数据集: {dataset_name}")
 
@@ -257,7 +260,7 @@ def main():
     parser.add_argument("--max-samples", type=int, default=None, help="最大处理样本数，用于测试")
     parser.add_argument("--num-threads", type=int, default=32, help="并行推理的线程数量，默认为 4")
     parser.add_argument("--prompt-type", type=str, default="reasoning with copy-paste", choices=["reasoning with copy-paste", "reasoning", "direct"], help="提示模板选择")
-    parser.add_argument("--dataset", type=str, default="hotpotqa", choices=["hotpotqa", "faitheval"], help="数据集名称，当前仅支持 hotpotqa")
+    parser.add_argument("--dataset", type=str, default="hotpotqa", choices=["hotpotqa", "faitheval", "pubmedqa"], help="数据集名称，当前仅支持 hotpotqa")
     parser.add_argument("--api-key", type=str, default="sk-lqztxtcbxxoonlmsxvdhllhdnoegywnvuhfnoqnxvpphrhkh", help="API Key，用于访问 第三方 服务")
     parser.add_argument("--temperature", type=float, default=0.7, help="模型生成温度")
     parser.add_argument("--top-p", type=float, default=0.95, help="模型生成 top-p 采样")
